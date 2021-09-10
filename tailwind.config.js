da317665d69575074d5ddefca8215b10c04c8d6c
Guilherme Marcondes <guisupre@gmail.com>
2021-09-10 11:17:07 +0100

    initial config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "ui-sans-serif", "system-ui", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
